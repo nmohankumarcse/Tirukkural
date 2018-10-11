ab2dfd210dc448a7c6cd8d401ec5f38bd963098a
Mohankumar <nmohankumar.cse@gmail.com>
2018-10-11 14:50:21 +0530

    first commit

---
+++ Tirukkral/SectionAndChapterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SectionAndChapterTableViewCell.swift
+//  Tirukkral
+//
+//  Created by Mohankumar on 17/11/17.
+//  Copyright © 2017 Mohan. All rights reserved.
+//
+
+import UIKit
+
+class SectionAndChapterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
